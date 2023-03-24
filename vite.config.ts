12b65196156cbf7f4d02bcc6a4cb1b1be6fcec14
HSQCoollaughing <1070404612@qq.com>
2023-03-24 17:40:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 //
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'hsqcoollaughing.github.io/FreeChatGPT/',
+  base: 'https://hsqcoollaughing.github.io/FreeChatGPT/',
   plugins: [react()],
   resolve: {
     alias: {
